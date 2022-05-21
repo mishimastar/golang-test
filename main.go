034553edc7554d1e3fa78c6e0f7652f93994a824
mishimastar <mishimastar3000@gmail.com>
2022-05-21 15:24:02 +0300

    Added logic for PUT requests, initialized struct for PUT queues

--- main.go
+++ main.go
@@ -6,21 +6,45 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"sync"
 )
 
 var address = "127.0.0.1:port"
+
+var PutQueues = struct {
+	sync.Mutex
+	queue map[string][]string
+}{queue: make(map[string][]string)}
 
 func main() {
 	port_to_listen := flag.String("p", "80", "port to listen")
 	flag.Parse()
 	address = strings.ReplaceAll(address, "port", *port_to_listen)
-	// fmt.Println("Inintial commit")
-	// router := http.NewServeMux
 	http.HandleFunc("/", myHandler)
 	fmt.Println("Listening on", address, "...")
 	log.Fatal(http.ListenAndServe(address, nil))
 }
 
 func myHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "PUT":
+		key_v := r.URL.Query().Get("v")
+		if len(key_v) == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+		} else {
+			if _, found := PutQueues.queue[r.URL.Path]; found {
+				w.WriteHeader(http.StatusOK)
+				PutQueues.Lock()
+				PutQueues.queue[r.URL.Path] = append(PutQueues.queue[r.URL.Path], key_v)
+				fmt.Println(PutQueues.queue, "appended", key_v, "to", r.URL.Path)
+				PutQueues.Unlock()
+			} else {
+				PutQueues.Lock()
+				PutQueues.queue[r.URL.Path] = []string{key_v}
+				fmt.Println(PutQueues.queue, "appended", key_v, "to created", r.URL.Path)
+				PutQueues.Unlock()
+			}
 
+		}
+	}
 }
