ab0c3e29ff4eb5925a0ed193aa4cd997efa9896a
mishimastar <mishimastar3000@gmail.com>
2022-05-21 15:03:44 +0300

    Inintialized server, added availability to spicify port to listen

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var address = "127.0.0.1:port"
 
 func main() {
-	fmt.Println("Inintial commit")
+	port_to_listen := flag.String("p", "80", "port to listen")
+	flag.Parse()
+	address = strings.ReplaceAll(address, "port", *port_to_listen)
+	// fmt.Println("Inintial commit")
+	// router := http.NewServeMux
+	http.HandleFunc("/", myHandler)
+	fmt.Println("Listening on", address, "...")
+	log.Fatal(http.ListenAndServe(address, nil))
+}
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+
 }
